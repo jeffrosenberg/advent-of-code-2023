a0041f56d55862c4aa0cdd195ce6817d8716bb04
Jeff Rosenberg <rosenberg.jeff@gmail.com>
2023-12-01 07:44:52 -0600

    Add utility for reading input files

---
+++ go/pkg/readaoc/readaoc.go
@@ -0,0 +1,25 @@
+package readaoc
+
+import (
+	"bufio"
+	"os"
+)
+
+func ReadAocInput(path string) (lines []string) {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	return
+}
---
+++ go/pkg/readaoc/readaoc_test.go
@@ -0,0 +1,15 @@
+package readaoc
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReadAocInput(t *testing.T) {
+	lines := ReadAocInput("../../../inputs/1.txt")
+	assert.Equal(t, 1000, len(lines))
+	assert.Equal(t, "2xjzgsjzfhzhm1", lines[0])
+	assert.Equal(t, "6ninexrxsvlmmzrsevenjhzzggfcxqrvfjtnjctveight9", lines[422])
+	assert.Equal(t, "9lgmxktj1frxl", lines[999])
+}
