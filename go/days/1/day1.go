3922d831154511f2bd00f15a79659d2d265842b3
Jeff Rosenberg <rosenberg.jeff@gmail.com>
2023-12-01 20:20:09 -0600

    Day 1 part 2 solution

--- go/days/1/day1.go
+++ go/days/1/day1.go
@@ -21,16 +21,14 @@ func main() {
 
 func extractCalibrationValue(input string) (int, error) {
 	value := 0
-	chars := []rune(input)
 	var digits []int
 
 	// iterate through and store digits
-	for i := 0; i < len(chars); i++ {
-		if isDigit(chars[i]) {
-			digit, err := strconv.Atoi(string(chars[i]))
-			if err != nil {
-				return 0, err
-			}
+	for i := 0; i < len(input); i++ {
+		if digit, isDigit := isDigit((input)[i]); isDigit {
+			digits = append(digits, digit)
+		}
+		if digit, isDigit := isSpelledDigit(input, i); isDigit {
 			digits = append(digits, digit)
 		}
 	}
@@ -42,7 +40,44 @@ func extractCalibrationValue(input string) (int, error) {
 	return value, nil
 }
 
-func isDigit(char rune) bool {
-	byt := byte(char)
-	return byt >= byte('0') && byt <= byte('9')
+func isDigit(char byte) (int, bool) {
+	if char >= byte('1') && char <= byte('9') {
+		digit, err := strconv.Atoi(string(char))
+		if err != nil {
+			panic(err) // Highly unlikely in these contrived examples!
+		}
+		return digit, true
+	}
+	return 0, false
+}
+
+func isSpelledDigit(input string, index int) (digit int, result bool) {
+	chars := input[index:]
+	numChars := len(input) - index
+
+	// this is brute-forced and not super elegant,
+	// but I'm not convinced making it fancier is any better
+	switch {
+	case numChars < 3:
+		return 0, false
+	case chars[:3] == "one":
+		return 1, true
+	case chars[:3] == "two":
+		return 2, true
+	case numChars >= 5 && chars[:5] == "three":
+		return 3, true
+	case numChars >= 4 && chars[:4] == "four":
+		return 4, true
+	case numChars >= 4 && chars[:4] == "five":
+		return 5, true
+	case chars[:3] == "six":
+		return 6, true
+	case numChars >= 5 && chars[:5] == "seven":
+		return 7, true
+	case numChars >= 5 && chars[:5] == "eight":
+		return 8, true
+	case numChars >= 4 && chars[:4] == "nine":
+		return 9, true
+	}
+	return 0, false
 }
--- go/days/1/day1_test.go
+++ go/days/1/day1_test.go
@@ -28,6 +28,34 @@ func TestExtractCalibrationValue(t *testing.T) {
 			input:    "treb7uchet",
 			expected: 77,
 		},
+		{
+			input:    "two1nine",
+			expected: 29,
+		},
+		{
+			input:    "eightwothree",
+			expected: 83,
+		},
+		{
+			input:    "abcone2threexyz",
+			expected: 13,
+		},
+		{
+			input:    "xtwone3four",
+			expected: 24,
+		},
+		{
+			input:    "4nineeightseven2",
+			expected: 42,
+		},
+		{
+			input:    "zoneight234",
+			expected: 14,
+		},
+		{
+			input:    "7pqrstsixteen",
+			expected: 76,
+		},
 	}
 
 	for _, test := range tests {
@@ -46,25 +74,35 @@ func TestExtractCalibrationValue(t *testing.T) {
 
 func TestIsDigit(t *testing.T) {
 	tests := []struct {
-		input    rune
-		expected bool
-		skip     bool
+		input        byte
+		expectedInt  int
+		expectedBool bool
+		skip         bool
 	}{
 		{
-			input:    'a',
-			expected: false,
+			input:        byte('a'),
+			expectedInt:  0,
+			expectedBool: false,
 		},
 		{
-			input:    '9',
-			expected: true,
+			input:        byte('9'),
+			expectedInt:  9,
+			expectedBool: true,
 		},
 		{
-			input:    't',
-			expected: false,
+			input:        byte('t'),
+			expectedInt:  0,
+			expectedBool: false,
 		},
 		{
-			input:    '0',
-			expected: true,
+			input:        byte('0'),
+			expectedInt:  0,
+			expectedBool: false,
+		},
+		{
+			input:        byte('1'),
+			expectedInt:  1,
+			expectedBool: true,
 		},
 	}
 
@@ -75,8 +113,69 @@ func TestIsDigit(t *testing.T) {
 
 		t.Run(string(test.input), func(t *testing.T) {
 			t.Log(test.input)
-			got := isDigit(test.input)
-			assert.Equal(t, test.expected, got)
+			gotInt, gotBool := isDigit(test.input)
+			assert.Equal(t, test.expectedInt, gotInt)
+			assert.Equal(t, test.expectedBool, gotBool)
+		})
+	}
+}
+
+func TestIsSpelledDigit(t *testing.T) {
+	tests := []struct {
+		name         string
+		input        string
+		index        int
+		expectedInt  int
+		expectedBool bool
+		skip         bool
+	}{
+		{
+			name:         "eightseventysix match eight",
+			input:        "eightseventysix",
+			index:        0,
+			expectedInt:  8,
+			expectedBool: true,
+		},
+		{
+			name:         "eightseventysix no match",
+			input:        "eightseventysix",
+			index:        1,
+			expectedInt:  0,
+			expectedBool: false,
+		},
+		{
+			name:         "eightseventysix match seven",
+			input:        "eightseventysix",
+			index:        5,
+			expectedInt:  7,
+			expectedBool: true,
+		},
+		{
+			name:         "eightseventysix match six",
+			input:        "eightseventysix",
+			index:        12,
+			expectedInt:  6,
+			expectedBool: true,
+		},
+		{
+			name:         "eightseventysix short circuit",
+			input:        "eightseventysix",
+			index:        13,
+			expectedInt:  0,
+			expectedBool: false,
+		},
+	}
+
+	for _, test := range tests {
+		if test.skip {
+			t.Skipf("Skipping %s", string(test.input))
+		}
+
+		t.Run(string(test.name), func(t *testing.T) {
+			t.Log(test.name)
+			gotInt, gotBool := isSpelledDigit(test.input, test.index)
+			assert.Equal(t, test.expectedInt, gotInt)
+			assert.Equal(t, test.expectedBool, gotBool)
 		})
 	}
 }
